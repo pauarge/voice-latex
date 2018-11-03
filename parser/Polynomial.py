ef91002467d6276a1ea6bac03b18200284a3b5bc
Pau Argelaguet <pau@outlook.com>
2018-11-03 23:04:12 +0100

    Parsing polynomials

--- parser/Polynomial.py
+++ parser/Polynomial.py
@@ -1,7 +1,7 @@
 from sympy import Symbol
 import re
 
-expr = r'([0-9]+)?(?:\s)?(times|divided)?(?:\s)?([a-zA-Z]+)(?:\s)?(squared|cubed)?(plus|minus)?'
+expr = r'([0-9]+)?(?:\s)?(times|divided)?(?:\s)?([a-zA-Z]+)(?:\s)?(squared|cubed)?(?:\s)?(plus|minus)?'
 
 
 class Polynomial:
@@ -18,16 +18,24 @@ class Polynomial:
 
     def parse(self):
         p = re.compile(expr)
-        groups = p.search(self.raw).groups()
+        groups = p.findall(self.raw)
+        parsed_groups = []
 
-        factor = int(groups[0]) if groups[0] else None
-        operation = groups[1]
-        variable = Symbol(groups[2])
-        power = groups[3]
+        for group in groups:
+            factor = int(group[0]) if group[0] else None
+            operation = group[1]
+            variable = Symbol(group[2])
+            power = group[3]
 
-        if factor and operation == 'times':
-            return factor * self.generate_without_factor(variable, power), variable
-        elif factor and operation == 'divided':
-            return factor / self.generate_without_factor(variable, power), variable
-        else:
-            return self.generate_without_factor(variable, power), variable
+            if factor and operation == 'times':
+                parsed_groups.append(factor * self.generate_without_factor(variable, power))
+            elif factor and operation == 'divided':
+                parsed_groups.append(factor / self.generate_without_factor(variable, power))
+            else:
+                parsed_groups.append(self.generate_without_factor(variable, power))
+
+        res = parsed_groups[0]
+        for i in range(1, len(groups)):
+            res = res + parsed_groups[i]
+
+        return res, variable
