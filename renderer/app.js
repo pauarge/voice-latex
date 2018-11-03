86adfd019e32431cf9bf8f6d7bf405fc572dba4d
Pau Argelaguet <pau@outlook.com>
2018-11-03 15:06:15 +0100

    Moving to ES16

--- renderer/app.js
+++ renderer/app.js
@@ -1,22 +1,26 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+app.get('/update', (req, res) => {
+    res.json({'status': 'ok'});
+});
+
+io.on('connection', function (socket) {
     console.log('a user connected');
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
     console.log('listening on *:3000');
 });
